d4ac4bb8d18dbb2d0f1776349b8f876be69d63a8
bangsuji <sjbang@lgupluspartners.co.kr>
2025-07-10 17:04:23 +0900

    [refactor] 객체 이름 변경..

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/dto/PointResponse.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/dto/PointResponse.java
@@ -3,7 +3,7 @@ package io.hhplus.tdd.dto;
 import io.hhplus.tdd.point.UserPoint;
 
 public record PointResponse(
-        long userId,
+        long id,
         long point
 ) {
     public static PointResponse from(UserPoint userPoint) {
